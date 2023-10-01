e31bdc9414db40ec6c9ba46f24b5e9a9d9437d16
Touhidul-MySoftHeaven <touhidul.developer.2022@gmail.com>
2023-10-01 18:42:33 +0600

    touhid change

--- src/java/com/servlets/SuccessServlet.java
+++ src/java/com/servlets/SuccessServlet.java
@@ -21,7 +21,19 @@ public class SuccessServlet extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
          response.setContentType("text/html");
         PrintWriter output=response.getWriter();
+        String name=request.getParameter("name");
+        String email=request.getParameter("email");
+        String password=request.getParameter("password");
+        String gender=request.getParameter("gender");
+        String cars=request.getParameter("cars");
+        String horns=request.getParameter("horns");
         output.println("<h3>Successfully Registered</h3>");
+        output.println("<h3>Name: "+name+"</h3>");
+        output.println("<h3>email: "+email+"</h3>");
+        output.println("<h3>password: "+password+"</h3>");
+        output.println("<h3>gender: "+gender+"</h3>");
+        output.println("<h3>cars: "+cars+"</h3>");
+        output.println("<h3>horns: "+horns+"</h3>");
     }
     
     
